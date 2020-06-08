772c0b54d3f7ce398d55ed4b7ea331bb2671d83a
swann <chevreauswann@outlook.fr>
2020-06-08 11:50:35 +0200

    quête sur les filtres

--- Shield.java
+++ Shield.java
@@ -17,15 +17,18 @@ public class Shield {
         heroes.add(new Hero("Hulk", 49, true));
         heroes.add(new Hero("Doctor Strange", 42, false));
 
-        List<Hero> elders;
-        // TODO 1 : filter heroes in order to found heroes older than 59
+        List<Hero> elders = heroes.stream()
+                .filter(age -> age.getAge() > 59)
+                .collect(Collectors.toList());
 
         System.out.println("\nElders:");
         for (Hero elder : elders) {
             System.out.println(elder.getName());
         }
 
-        List<Hero> intolerants;
+        List<Hero> intolerants = heroes.stream()
+                .filter(glutenIntolerent -> glutenIntolerent.isGlutenIntolerant() == true)
+                .collect(Collectors.toList());
         // TODO 2 : filter heroes in order to found heroes that are gluten intolerants
 
         System.out.println("\nGluten intolerants:");
